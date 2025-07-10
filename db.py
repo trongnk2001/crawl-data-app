6d069604160bb701e45590b98d917baac23994ca
trongnk2001 <nguyenkhactrong01@gmail.com>
2025-07-10 16:49:35 +0700

    remove file unused

--- db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import config
-
-engine = create_engine(config.DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
